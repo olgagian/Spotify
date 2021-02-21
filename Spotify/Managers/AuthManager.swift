321b95811dafff80880afb6b1ebb7d9a1acc33a6
ioannis <olgagian@otenet.gr>
2021-02-22 01:09:36 +0200

    authomanager

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -12,11 +12,19 @@ final class AuthManager {
     struct Constants {
         static let clientID = "b11690406e7740deaa6b888e57aeaa85"
         static let clientSecret = "0da9bfc3a10a4523ab7cde1222487d76"
+        static let tokenAPIURL = "https://accounts.spotify.com/api/token"
     }
     
     private init() {}
     var isSignedIn: Bool {
         return false
+    }
+    public var signInURL:URL?{
+        let scopes = "user-read-private"
+        let redirectURI =  "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
     }
     private var accessToken: String? {
         return nil
@@ -30,5 +38,42 @@ final class AuthManager {
     }
     private var shouldRefreshToken:Bool {
         return false
+    }
+    public func exchangeCodeForToken(code:String,completion:@escaping((Bool)-> Void)) {
+        
+        //Get Token
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+        var components =  URLComponents()
+        components.queryItems = [URLQueryItem(name :"grant_type", value: "authorization_code"),
+                                 URLQueryItem(name :"code", value: code),
+                                 URLQueryItem(name :"redirect_uri", value: "https://www.iosacademy.io")]
+        var request = URLRequest(url:url)
+        request.httpMethod = "POST"
+        request.httpBody = components.query?.data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) {data, _, error in
+            guard let data = data , error == nil else {
+                completion(false)
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print("SUCCESS:\(json)")
+            }
+            catch {
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
+    }
+    private func RefreshAccessToken() {
+        
+    }
+    private func cacheToten(){
+        
     }
 }
--- Spotify/New Group/Controllers/Other/AuthViewController.swift
+++ Spotify/New Group/Controllers/Other/AuthViewController.swift
@@ -25,13 +25,34 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         view.backgroundColor = .systemBackground
         webView.navigationDelegate = self
         view.addSubview(webView)
-        
+        guard let url = AuthManager.shared.signInURL else {
+            return
+        }
+        webView.load(URLRequest(url:url))
 
 
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         webView.frame = view.bounds
+    }
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        guard let url = webView.url else {
+            return
+        }
+        //Exchange the code for access token
+        guard let code = URLComponents(string:url.absoluteString)?.queryItems?.first(where:{$0.name == "code"})?.value
+        else {
+            return
+        }
+        webView.isHidden = true
+        print("Code:\(code)")
+        AuthManager.shared.exchangeCodeForToken(code: code) {[weak self] success in
+            DispatchQueue.main.async {
+                self?.navigationController?.popToRootViewController(animated: true)
+                self?.completionHandler?(success)
+            }
+        }
     }
     
 
