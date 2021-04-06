eb7583a7cb4d8b3c4f553fbb8b3d1a282660270d
ioannis <olgagian@otenet.gr>
2021-04-06 03:18:23 +0300

    profileViewController first configuration

--- Spotify/New Group/Controllers/Other/SettingsViewController.swift
+++ Spotify/New Group/Controllers/Other/SettingsViewController.swift
@@ -14,7 +14,8 @@ class SettingsViewController: UIViewController,UITableViewDelegate,UITableViewDa
                            forCellReuseIdentifier: "cell")
         return tableView
     }()
-
+    
+    
     private var sections = [Section]()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,59 +26,62 @@ class SettingsViewController: UIViewController,UITableViewDelegate,UITableViewDa
         tableView.dataSource = self
         tableView.delegate = self
     }
-    private func configureModels(){
+    private func configureModels() {
         sections.append(Section(title: "Profile", options: [Option(title: "View Your Profile", handler: { [weak self] in
-                    DispatchQueue.main.async {
-                        self?.viewProfile()
-                    }
-                })]))
-
-                sections.append(Section(title: "Account", options: [Option(title: "Sign Out", handler: { [weak self] in
-                    DispatchQueue.main.async {
-                        self?.signOutTapped()
-                    }
-                })]))
+            DispatchQueue.main.async {
+                self?.viewProfile()
             }
+        })]))
+        
+        sections.append(Section(title: "Account", options: [Option(title: "Sign Out", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.signOutTapped()
+            }
+        })]))
+    }
     private func signOutTapped() {
         
     }
-   
-  
+    
+    
     private func viewProfile() {
         let vc = ProfileViewController()
         vc.title = "Profile"
         vc.navigationItem.largeTitleDisplayMode = .never
-        vc.navigationController?.pushViewController(vc, animated: true)
+        navigationController?.pushViewController(vc, animated: true)
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
     }
-//MARK - TableView
+   
+    
+    
+    //MARK - TableView
     func numberOfSections(in tableView: UITableView) -> Int {
-            return sections.count
-        }
-
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return sections[section].options.count
-        }
-
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let model = sections[indexPath.section].options[indexPath.row]
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-            cell.textLabel?.text = model.title
-            return cell
-        }
-
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            tableView.deselectRow(at: indexPath, animated: true)
-            // Call handler for cll
-            let model = sections[indexPath.section].options[indexPath.row]
-            model.handler()
-        }
-
-        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-            let model = sections[section]
-            return model.title
-        }
+        return sections.count
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = sections[indexPath.section].options[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Call handler for cll
+        let model = sections[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let model = sections[section]
+        return model.title
+    }
+}
